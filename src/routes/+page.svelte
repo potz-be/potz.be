68d84f61b2e37a2fe1eeaf56f8abe9b3249a9cd7
Jens Pots <jens@jenspots.com>
2024-01-15 11:29:42 +0100

    feat: placeholder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col gap-4 justify-center items-center w-screen h-screen">
+	<img src="/potz.png" class="w-24 h-24" alt="POTZ logo" />
+	<p class="opacity-80">Onze website wordt momenteel aangepast.</p>
+	<a href="https://topradio.be" id="topradio-link">
+		<div class="flex gap-1">
+			<p>Luister via TOPradio.be</p>
+			<i class="bi bi-chevron-right"></i>
+		</div>
+	</a>
+</div>
+
+<style lang="postcss">
+	:root {
+		@apply bg-neutral-900 text-white;
+	}
+
+	#topradio-link {
+		@apply pl-4 pr-2 py-2 bg-indigo-800 font-medium rounded-full text-xs;
+	}
+</style>
